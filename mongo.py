d6990000dbacdb1b0c30452c3f39582373b688bd
gald33 <gald33@gmail.com>
2017-03-31 18:56:39 +0300

    Add files via upload

---
+++ mongo.py
@@ -0,0 +1,95 @@
+from pymongo import MongoClient
+from pprint import pprint
+
+
+class DatabaseConnection:
+    client = MongoClient('mongodb://prices_db_user:okmjmkol@ds137090.mlab.com:37090/prices_db')
+    db = client.prices_db
+
+    def __init__(self):
+        pass
+
+
+class Database:
+    def __init__(self):
+        self.db = DatabaseConnection.db
+
+    def write_pricelists_one_by_one(self, pricelists):   #slow but with indication
+        length = len(pricelists)
+        for i, pricelist in enumerate(pricelists):
+            print 'save pricelists', i+1, 'of', length, ': branch code', \
+                pricelist["branch_code"], 'date', pricelist["date"]
+            self.db.priclists.update_one(
+                {"_id": pricelist["_id"]},
+                {
+                    "$set": pricelist
+                },
+                upsert=True
+            )
+
+
+    def write_one_pricelist(self, pricelist):
+        self.db.priclists.update_one(
+            {"_id": pricelist["_id"]},
+            {
+                "$set": pricelist
+            },
+            upsert=True
+        )
+
+
+    def write_pricelists_of_date(self, date, pricelists):   #another approach I might try
+        self.db.priclists.update_many(
+            {"date": date},
+            {
+                "$set": pricelist
+            },
+            upsert=True
+        )
+
+
+    def write_pricelists(self, pricelists): #too slow with no progress indication
+        try:
+            self.db.priclists.insert_many(pricelists)
+        except:
+            print "error writing to db"
+
+    def read_pricelists(self):
+        pricelists = []
+        results = self.db.priclists.find()
+        for doc in results:
+            pricelists.append(doc)
+            print 'load doc', doc["_id"]
+
+    def write_items(self, items):
+        self.db.items.update_one(
+            {"name": "items"},
+            {
+                "$set": {"data": items}
+            },
+            upsert=True
+        )
+
+
+    def read_items(self):
+        return self.db.items.find_one({"name":"items"})["data"]
+
+
+    def write_barcode_super_category_position(self, barcode_super_category_position):
+        self.db.cluster.update_one(
+            {"name": "barcode_super_category_position"},
+            {
+                "$set": {"data": barcode_super_category_position}
+            },
+            upsert=True
+        )
+
+
+    def read_barcode_super_category_position(self):
+        return self.db.cluster.find_one({"name":"barcode_super_category_position"})["data"]
+
+
+
+if __name__ == '__main__':
+    db = Database()
+    db.read_pricelists()
